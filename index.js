44e88368b7ac687a895186aac1d829024d544d3d
Prem Sai <55195257+premsaiappasani@users.noreply.github.com>
2021-10-30 13:54:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.get("/home",()=>{
+  console.log("home page");
+  res.send("<h1>This is Home Page</h1>");
+})
+
 app.get("/",()=>{
   console.log("req recived");
   res.send("Done Loading!!");
 });
 
-app.get("/home",()=>{
-  console.log("home page");
-  res.send("<h1>This is Home Page</h1>");
-})
+
 
 app.listen(1000,()=>{
   console.log("listening to port 1000");
